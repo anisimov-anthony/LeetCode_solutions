33b6d12077a970ef4df6daddc3c7a8b3fabba0e5
AntonAnisimovMIPT <anisimov-anton-main@mail.ru>
2025-04-26 18:37:24 +0300

    added 22

---
+++ rust/src/solutions/sol_0022_generate_parentheses.rs
@@ -0,0 +1,31 @@
+#[allow(dead_code)]
+pub fn generate_parenthesis(n: i32) -> Vec<String> {
+    fn back_track(s: String, open: i32, close: i32) -> Vec<String> {
+        let mut res = vec![];
+        if open == 0 && close == 0 {
+            return vec![s];
+        }
+        if open > 0 {
+            res.append(&mut back_track(s.clone() + "(", open - 1, close + 1));
+        }
+        if close > 0 {
+            res.append(&mut back_track(s.clone() + ")", open, close - 1));
+        }
+        res
+    }
+    back_track("".to_string(), n, 0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_parenthesis_basic() {
+        assert_eq!(
+            generate_parenthesis(3),
+            vec!["((()))", "(()())", "(())()", "()(())", "()()()"]
+        );
+        assert_eq!(generate_parenthesis(1), vec!["()"]);
+    }
+}
